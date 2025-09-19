3d303461530fec40ac306b117e2f8d0793762848
yabluninn <yablunin.artem@gmail.com>
2025-09-19 22:14:53 +0300

    remove new api file

---
+++ lib/api/api.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const baseURL = `${process.env.NEXT_PUBLIC_API_URL}/api`;
+
+export const api = axios.create({
+  baseURL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
